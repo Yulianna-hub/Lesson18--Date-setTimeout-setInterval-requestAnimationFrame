376fa30d4c4ce28c131c1ac98edcc8e852fd5990
Yuliia Borys <yuliannaboris@gmail.com>
2020-04-29 19:46:59 +0200

    script.js

--- script.js
+++ script.js
@@ -1,96 +1,102 @@
 window.addEventListener('DOMContentLoaded', function() {
     'use strict';
-    //timer
-    function countTimer(deadline) {
-    let timerHours = document.querySelector('#timer-hours'),
-        timerMinutes = document.querySelector('#timer-minutes'),
-        timerSeconds = document.querySelector('#timer-seconds');
+//timer
+function countTimer(deadline) {
+let timerHours = document.querySelector('#timer-hours'),
+    timerMinutes = document.querySelector('#timer-minutes'),
+    timerSeconds = document.querySelector('#timer-seconds');
     
-        function getTimeRemaining() {
-        let dateStop = new Date(deadline).getTime(),
-            dateNow = new Date().getTime(),
-            timeRemaining = (dateStop - dateNow) / 1000,
-            seconds = Math.floor(timeRemaining % 60),
-            minutes = Math.floor((timeRemaining / 60) % 60),
-            hours = Math.floor(timeRemaining / 60 / 60);
-            return {timeRemaining, hours, minutes, seconds};
-        }
-        
-        const interval = setInterval(() => {
-            let timer = getTimeRemaining();  
-                timer.hours > 9 ? timerHours.textContent = timer.hours : timerHours.textContent = '0' +  timer.hours;
-                timer.minutes > 9 ? timerMinutes.textContent = timer.minutes : timerMinutes.textContent = '0' + timer.minutes;
-                timer.seconds > 9 ? timerSeconds.textContent = timer.seconds : timerSeconds.textContent = '0' + timer.seconds;
-    
-        if (timer.timeRemaining < 0) {
-            clearInterval(interval);
-            timerHours.textContent = '00';
-            timerMinutes.textContent ='00';
-            timerSeconds.textContent = '00';
-    
-            }
-        }, 1000);
+function getTimeRemaining() {
+let dateStop = new Date(deadline).getTime(),
+    dateNow = new Date().getTime(),
+    timeRemaining = (dateStop - dateNow) / 1000,
+    seconds = Math.floor(timeRemaining % 60),
+    minutes = Math.floor((timeRemaining / 60) % 60),
+    hours = Math.floor(timeRemaining / 60 / 60);
+    return {timeRemaining, hours, minutes, seconds};
+}
+
+const interval = setInterval(() => {
+    let timer = getTimeRemaining();  
+        timer.hours > 9 ? timerHours.textContent = timer.hours : timerHours.textContent = '0' +  timer.hours;
+        timer.minutes > 9 ? timerMinutes.textContent = timer.minutes : timerMinutes.textContent = '0' + timer.minutes;
+        timer.seconds > 9 ? timerSeconds.textContent = timer.seconds : timerSeconds.textContent = '0' + timer.seconds;
+
+if (timer.timeRemaining < 0) {
+    clearInterval(interval);
+    timerHours.textContent = '00';
+    timerMinutes.textContent ='00';
+    timerSeconds.textContent = '00';
+
     }
-    countTimer('30 april 2020');
+}, 1000);
+}
+countTimer('30 april 2020');
+
+//menu
+const toggleMenu = () => {
+const btnMenu = document.querySelector('.menu'),
+        menu = document.querySelector('menu'),
+        closeBtn = document.querySelector('.close-btn'),
+        menuItems = menu.querySelectorAll('ul>li');
+
+const handlerMenu = () => {
+    menu.classList.toggle('active-menu'); 
+};
     
-    //menu
-    const toggleMenu = () => {
-    const btnMenu = document.querySelector('.menu'),
-          menu = document.querySelector('menu'),
-          closeBtn = document.querySelector('.close-btn'),
-          menuItems = menu.querySelectorAll('ul>li');
+btnMenu.addEventListener('click', handlerMenu);
+closeBtn.addEventListener('click', handlerMenu);
+menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+
+};
+toggleMenu();
+//popup
+const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+    popupBtn = document.querySelectorAll('.popup-btn'),
+    popUpClose = document.querySelector('.popup-close'),
+    popupContent = document.querySelector('.popup-content');
+
+popupBtn.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        if (screen.width  < 768) {
+        popup.style.display = 'block';
+    }else {
+        popup.style.display = 'block';
+        popUpAnimation();
+    }
+    });
     
-    const handlerMenu = () => {
-        menu.classList.toggle('active-menu'); 
-    };
-      
-    btnMenu.addEventListener('click', handlerMenu);
-    closeBtn.addEventListener('click', handlerMenu);
-    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+});      
+popUpClose.addEventListener('click', () => {
+    popup.style.display = 'none';
+    popupContent.style.top = '';
     
-    };
-    toggleMenu();
-    //popup
-    const togglePopUp = () => {
-        const popup = document.querySelector('.popup'),
-        popupBtn = document.querySelectorAll('.popup-btn'),
-        popUpClose = document.querySelector('.popup-close'),
-        popupContent = document.querySelector('.popup-content');
+});
     
-        popupBtn.forEach((elem) => {
-            elem.addEventListener('click', () => {
-                popup.style.display = 'block';
-                   
-                if (screen.width  > 768) {
-                    !popUpAnimation();
-                  } else { 
-                    popUpAnimation();
-                  }
-           
-            });
-    
-        });
-        popUpClose.addEventListener('click', () => {
-            popup.style.display = 'none';
-        });
-    
-    //popUpAnimation
-    let popUpAnimation = function () {
+//popUpAnimation
+let popUpAnimation = function() {
+    const topPosition = (document.documentElement.clientHeight - popupContent.clientHeight) / 2;
     let start = Date.now();
     let timer = setInterval(function() {
-    let timePassed = Date.now() - start;
-      if (timePassed >= 3000) {
-        clearInterval(timer);
-        return;
-      }
-    muve(timePassed);
-    }, 15);
+        console.log(popupContent.style.top);
+        console.log(topPosition);
+        let timePassed = Date.now() - start;
+        if (parseFloat(popupContent.style.top) >= topPosition) {
+            clearInterval(timer);
+            return;
+        }
+        muve(timePassed);
+    }, 10);
+
     function muve(timePassed) {
-        popupContent.style.top = timePassed / 10 + 'px';
+        popupContent.style.top = timePassed / 5 + 'px';
     }
-    };
     
-    };
-    togglePopUp();
-    
-    });
+};
+
+};
+togglePopUp();
+
+});
+  
