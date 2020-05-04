61acd3f4a2e7ac2018749428ff082308593a3ca9
Yuliia Borys <yuliannaboris@gmail.com>
2020-05-04 19:24:31 +0200

    script.js

--- script.js
+++ script.js
@@ -213,24 +213,62 @@ window.addEventListener('DOMContentLoaded', function () {
                 startSlide();           
             }
         });
-
         startSlide(2000);
     };
-
     slider();
 //calculeta 
-   const calculeta = () => {
+   const calculation = (price = 100) => {
         const calcBlock = document. querySelector('.calc-block');
         const calcItems = document.querySelectorAll('.calc-item');
-        const total = document.querySelector('#total');
+        const calcType = document.querySelector('.calc-type');
+        const calcSquare = document.querySelector('.calc-square');
+        const calcDay = document.querySelector('.calc-day');
+        const calcCount = document.querySelector('.calc-count');
+        const totalValue = document.querySelector('#total');
 
-        const calcRes = () => {
+        const calcRes = () => {  
+            let total = 0;
+            let countValue = 1;
+            let dayValue = 1; 
             calcItems.forEach((elem) =>elem.value.match(/d/gi, ''));
-            total.textContent = calcItems.value;  
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+            const squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value -1) / 10;
+                console.log(calcCount.value);
+            }
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;    
+            }else if (calcDay.value && calcDay < 10) {
+               dayValue *= 1.5; 
+            }
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+            totalValue.textContent = calcItems.value;
+            totalValue.textContent = total;
         };
-        calcBlock.addEventListener('input', calcRes());
+        //calcBlock.addEventListener('input', calcRes());
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+            //вариант 1
+            /*if (target.matches('.calc-type') || target.matches('.calc-square') ||
+            target.matches('.calc-day') || target.matches('.calc-count')) {
+                calcRes(); 
+            }*/
+            //вариант 2
+            /*if (target === calcType || target === calcSquare ||
+                target === calcDay || target === calcCount) {
+                    calcRes();
+            }*/
+            //вариант 3
+           if (target.matches('select') || target.matches('input')) {
+                calcRes();
+            }
+        });   
     };
-    calculeta();
+    calculation(100);
 
  //our command
     const ourCommand = () => {
